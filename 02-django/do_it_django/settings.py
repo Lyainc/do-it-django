c0cee136d24ac42e58680be02d734284d5518f12
Lyainc <46391424+Lyainc@users.noreply.github.com>
2021-08-28 01:43:54 +0900

    django server setting

--- 02-django/do_it_django/settings.py
+++ 02-django/do_it_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'single_pages',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'do_it_django.urls'
