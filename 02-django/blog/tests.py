dc9c35beea2784bdaa8554b3bccafab52fbe3363
Lyainc <46391424+Lyainc@users.noreply.github.com>
2021-09-11 23:00:51 +0900

    navbar, footer module화

--- 02-django/blog/tests.py
+++ 02-django/blog/tests.py
@@ -7,6 +7,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_bnt = navbar.find('a', text='Home')
+        self.assertEqual(home_bnt.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         # 1.1 포스트 목록 불러오기
         response = self.client.get('/blog/')
@@ -16,10 +33,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
         # 1.4 네비게이션 바
-        navbar = soup.nav
-        # 1.5 blog, about me라는 문구가 네비게이션 바에 출력
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -66,9 +80,7 @@ class TestView(TestCase):
 
         # 2.2 포스트 목록 페이지와 같은 네비게이션 바가 있다
 
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 있다
         self.assertIn(post_001.title, soup.title.text)
@@ -76,7 +88,7 @@ class TestView(TestCase):
         # 2.4 첫 번째 포스트의 제목이 포스트 영역에 있다
         main_area = soup.find('div', id='main_area')
         post_area = main_area.find('div', id='post_area')
-        self.assertIn(post_area.title, post_area.text)
+        self.assertIn(post_001.title, post_area.text) # post_area.title -> 오타 같음
 
         # 2.5 첫 번째 포스트의 작성자가 포스트 영역에 있다(미구현)
         # 2.6 첫 번째 포스트의 내용이 포스트 영역에 있다.
