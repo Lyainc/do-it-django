b54683953e72c191f3c1688cf89325de089bfb88
Lyainc <koore12@gmail.com>
2023-10-28 00:30:51 +0900

    블로그 포스트 목록 페이지 작업

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post
     template_name = 'blog/index.html'
     ordering = '-pk'
+    
+class PostDetail(DetailView):
+    model = Post
