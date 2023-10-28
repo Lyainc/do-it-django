19ac152bb62bf6b7c2fbbae6e85ab91223d45b53
Lyainc <koore12@gmail.com>
2023-10-28 22:02:00 +0900

    테스트 데이터 입력

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=20)
-    content = models.TextField
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     # author
     # tags
     
