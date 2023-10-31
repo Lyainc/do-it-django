b0d9518ac8c28c05f0cc96b8586c3095cb59e816
Lyainc <koore12@gmail.com>
2023-11-01 00:31:05 +0900

    포스팅 상세 내용 테스트 코드 작업

--- blog/tests.py
+++ blog/tests.py
@@ -56,3 +56,35 @@ class TestView(TestCase):
         
         # 3.4 아직 게시물이 없습니다. 라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+        
+    def test_post_detail(self):
+        # 1.1 포스트가 하나 있다.
+        post_001 = Post.objects.create(
+            title = '첫 번째 포스트입니다.',
+            content = '첫 번째 콘텐츠의 콘텐츠입니다.',
+        )
+        # 1.2 포스트의 URL은 '/blog/1/'이다.
+        self.assertEqual(post_001.get_absolute_url(), "/blog/1/")
+        
+        # 2.1 첫번째 포스트의 URL로 접근하면 정상적으로 작동한다
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # 2.2 포스트 목록 페이지와 같은 네비게이션 바가 있다.
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+        
+        # 2.3 첫번째 포스트의 제목이 웹브라우저 탭 타이틀에 있다.
+        self.assertIn(post_001.title, soup.title.text)
+        
+        # 2.4 첫번째 포스트의 제목이 포스트 영역에 있다.
+        main_area = soup.find('div', id = 'main_area')
+        post_area = main_area.find('div', id= 'post_area')
+        self.assertIn(post_001.title, post_area.text)
+                
+        # 2.5 첫번째 포스트의 작성자(Author)가 있다.
+    
+        # 2.6 첫번째 포스트의 내용(Content)가 있다.
+        sefl.assertIn(post_001.content, post_area.text)
