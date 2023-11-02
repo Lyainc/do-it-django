9499b2db274233610447a5e4abb97755fa8f17b4
Lyainc <koore12@gmail.com>
2023-11-03 01:04:55 +0900

    네비바, 푸터 모듈화

--- blog/tests.py
+++ blog/tests.py
@@ -20,11 +20,9 @@ class TestView(TestCase):
         self.assertEqual(soup.title.text, 'Blog')
         
         # 1.4 네비바가 있다
-        navbar = soup.nav
-        
         # 1.5 Blog, About Me라는 문구가 네비게이션 바에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        
+        self.navbar_test(soup)
         
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -72,9 +70,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         
         # 2.2 포스트 목록 페이지와 같은 네비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        self.navbar_test(soup)
         
         # 2.3 첫번째 포스트의 제목이 웹브라우저 탭 타이틀에 있다.
         self.assertIn(post_001.title, soup.title.text)
@@ -88,3 +84,20 @@ class TestView(TestCase):
     
         # 2.6 첫번째 포스트의 내용(Content)가 있다.
         self.assertIn(post_001.content, post_area.text)
+        
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+    
+        logo_btn = navbar.find('a', text="Blog Test Page")
+        self.assertEqual(logo_btn.attrs['href'], '/')
+        
+        home_btn = navbar.find('a', text="Home")
+        self.assertEqual(home_btn.attrs['href'], '/')
+        
+        blog_btn = navbar.find('a', text="Blog")
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')        
+        
+        about_me_btn = navbar.find('a', text="About Me")
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
