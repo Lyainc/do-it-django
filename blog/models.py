d6b9d3b1c5e5ecb2f6f5b579b48b65ac7418ae5a
Lyainc <koore12@gmail.com>
2023-12-16 23:49:32 +0900

    태그 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,16 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
 class Post(models.Model):
     title = models.CharField(max_length=20)
     subtitle = models.CharField(max_length=50)
@@ -29,6 +39,7 @@ class Post(models.Model):
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
     
     def __str__(self):
         return f'({self.pk}) {self.title} :: {self.author}'
@@ -41,4 +52,3 @@ class Post(models.Model):
     
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
-    
